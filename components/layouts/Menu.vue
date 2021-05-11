0516f15bf2177f4af0459886a133d9e08c9c7556
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-12 08:39:10 +0900

    add new menu component

---
+++ components/layouts/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+<div class='component-root'>
+  <div class='menu-btn' @click='onClickedMenuBtn'>
+    <img src='~assets/icons/commons/menu.svg' alt='menu icon'>
+  </div>
+  <div class='menu-title'>
+    {{ title }}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    title: String
+  },
+  methods: {
+    onClickedMenuBtn() {
+      // TODO("メニューを表示
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.component-root {
+  background: linear-gradient(121deg, rgba(37, 60, 128, 0.8) 0%, rgba(66, 106, 224, 0.8) 57%, rgba(15, 188, 249, 0.8) 100%), url('~assets/logos/logo.svg') center center/80% no-repeat;
+}
+</style>
