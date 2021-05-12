e3403b4c25090e682699a23684d8d7fb867aa1ca
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-12 23:11:46 +0900

    component props class

---
+++ scripts/types/componentprops/IconTextButton.js
@@ -0,0 +1,14 @@
+export default class IconTextButton {
+  title: String
+  icon: String
+
+  /**
+   * IconTextButton
+    * @param {String} title
+   * @param {String} icon
+   */
+  constructor(title, icon) {
+    this.title = title
+    this.icon = icon
+  }
+}
