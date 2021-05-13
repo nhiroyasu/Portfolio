7602b7f6c5072e122df8546bee11c406c0e10591
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-13 23:33:16 +0900

    add new component

---
+++ components/ui/MacLikeIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component-root">
+    <div class="icon">
+      <img :src="img" alt="icon" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MacLikeIcon',
+  props: {
+    img: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
