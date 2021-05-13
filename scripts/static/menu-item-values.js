0defbfe4c14e711210c939999833a99d2fbf1349
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-13 23:32:18 +0900

    update static value

---
+++ scripts/static/menu-item-values.js
@@ -0,0 +1,28 @@
+import MenuItem from '@/scripts/types/componentprops/MenuItem'
+
+/**
+ * Menu Item Values
+ * @type {Array<MenuItem>}
+ */
+export const menuItemValues = [
+  {
+    title: 'HOME',
+    link: '/',
+    icon: require('@/assets/icons/commons/home.svg'),
+  },
+  {
+    title: 'ABOUT',
+    link: '/about',
+    icon: require('@/assets/icons/commons/cyan logo white.svg'),
+  },
+  {
+    title: 'APPLICATION',
+    link: '/apps',
+    icon: require('@/assets/icons/commons/apps.svg'),
+  },
+  {
+    title: 'SNS',
+    link: '/sns',
+    icon: require('@/assets/icons/commons/chat.svg'),
+  },
+]
