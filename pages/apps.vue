a7beb43b067e928ddef4f60f7a6582126b9565ea
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-19 08:43:26 +0900

    init page

---
+++ pages/apps.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Apps</div>
+</template>
+
+<script>
+export default {
+  name: 'apps',
+}
+</script>
+
+<style scoped></style>
---
+++ pages/sns.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>SNS</div>
+</template>
+
+<script>
+export default {
+  name: 'sns',
+}
+</script>
+
+<style scoped></style>
