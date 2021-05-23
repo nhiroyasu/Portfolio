2cdb61ce7534ed8bc226990d6e57fb1a12712565
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-23 17:01:19 +0900

    for display text

--- scripts/types/commons/Duration.js
+++ scripts/types/commons/Duration.js
@@ -1,3 +1,5 @@
+import { formatDate } from '@/scripts/functions/data-formatting'
+
 /**
  * 期間を表すクラス
  */
@@ -23,5 +25,18 @@ export default class Duration {
   constructor(start, end) {
     this.start = start
     this.end = end
+  }
+
+  /**
+   * 表示用の文字列を返す
+   * ex) 2020/11/22 ~ 2021/05/03
+   * @returns {String}
+   */
+  toStringForDisplay() {
+    const format = 'yyyy年MM月dd日'
+    const startDate = formatDate(this.start, format)
+    const endDate =
+      this.end !== null ? ` ~ ${formatDate(this.end, format)}` : ''
+    return startDate + endDate
   }
 }
