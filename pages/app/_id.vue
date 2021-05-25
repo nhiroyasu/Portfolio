94028f3d824a0d84c9c237d2cc74feac705ffba6
cyan0fbcf9 <cyan1109nh@gmail.com>
2021-05-25 09:33:08 +0900

    application presentation

---
+++ pages/app/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-root">
+    {{ appId }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'present_app',
+  async asyncData({ route }) {
+    return {
+      appId: route.params.id,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
